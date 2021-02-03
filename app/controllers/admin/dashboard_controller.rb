5291fbaea1071bb1911c7f63f431fa58c0b74bda
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-03 21:17:15 +0000

    Added Feature: Admin Dashboard - Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,14 @@
 class Admin::DashboardController < ApplicationController
 
-  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'].to_s, password: ENV['ADMIN_PASSWORD'].to_s, except: :index
+
 
   def show
+
+    # Display a count of how many products are in the database
+    @products_count = Product.count(:all)
+    # Display a count of how many categories are in the database
+    @categories_count = Category.count(:all)
+
   end
 end
